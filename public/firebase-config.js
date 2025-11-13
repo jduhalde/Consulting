f676296af140864c1e3846bb4ac191d1e55439ce
jduhal <50839605+jduhal@users.noreply.github.com>
2025-11-13 02:49:12 -0300

    Add: firebase-config.js para inicializar Firebase

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNc_dzWHx9L83VboaWtOb8bGGd_bOH2zA",
+    authDomain: "jd-consulting.firebaseapp.com",
+    projectId: "jd-consulting",
+    storageBucket: "jd-consulting.appspot.com",
+    messagingSenderId: "168091751045",
+    appId: "1:168091751045:web:5ba8cdb578345da7578fec"
+};
